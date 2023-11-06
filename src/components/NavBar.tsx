0ec0d9f091463fd056dba2250f616d13cbb90040
moses ka <you@example.com>
2023-11-06 22:36:25 +0100

    better names

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,12 +13,15 @@ export default function NavBar() {
           </div>
 
           <div id="search-dev" className="flex md:order-1">
-            <button
+            <button  id="navbar-search-toggle"
               type="button"
+             
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100
+               dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200
+                dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
             >
               <svg
                 className="w-5 h-5"
@@ -37,7 +40,8 @@ export default function NavBar() {
               </svg>
               <span className="sr-only">Search</span>
             </button>
-            <div className="relative hidden md:block">
+            <div id="search-input" 
+            className="relative hidden md:block">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -65,7 +69,7 @@ export default function NavBar() {
                 placeholder="Search..."
               />
             </div>
-            <button
+            <button id="navbar-collapse-toggle"
               data-collapse-toggle="navbar-search"
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500
@@ -92,10 +96,11 @@ export default function NavBar() {
               </svg>
             </button>
           </div>
-          <div
-            id="navbar-collapse"
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          <div id="navbar-collapse"
+           
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" 
           >
+            {/* here to unhide the nav bar items  */}
             <ul
               className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg
              bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800
