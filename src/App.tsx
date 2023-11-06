718accecbbcd1e336c8ac423fa82f205d649e331
moses ka <you@example.com>
2023-11-06 22:26:08 +0100

    first commit NavBar in prog

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar"
+
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar/>
+      <h1 className="  text-red-900  text-center"></h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import Hello from './components/Hello';
+ReactDOM.render(
+  <StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/hello" element={<Hello />} />
+    </Routes>
+  </Router>,
+  </StrictMode>,
+  document.getElementById('root')
+);
